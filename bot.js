dd7b9144dad929799b160c5c22f8eaed5e95e65b
Keung Huang <notinschool@gmail.com>
2017-09-23 12:46:05 -0700

    switched from https module to request-promise for imgur api calls

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const https = require('https');
+var rp = require('request-promise');
 const pg = require('pg');
 
 const auth = require('./auth.json');
@@ -31,7 +31,7 @@ function getRandomInt(min, max) {
 		if (allowNSFW && currentLink.nsfw === true)
 			return currentLink.url;
 		else
-			return `You cant handle this NSFW img, bro! Here is an unrendered link:\n${currentLink.url.substring(7, currentLink.url.length)}`;
+			return `This image is NSFW! Here is an unrendered link:\n${currentLink.url.substring(7, currentLink.url.length)}`;
 	}
 }
 
@@ -48,110 +48,113 @@ function makeCacheObject(json){
 var bot = new Discord.Client();
 
 bot.on('ready', () => {
-  console.log('Bot is ready');
-	//console.log(process.env);
-
   //connect to database
 	pg.connect(process.env.DATABASE_URL , function(err, client, done) {
 		client.query('SELECT item FROM restrict_list', function(err, result) {
 		  done();
-		  if (err)
-		   { console.error(err);}
-		  else
-		   {
-			restrictList = result.rows.map( row => row.item ) || [];
+		  if (err){
+        console.error(err);}
+		  else {
+        restrictList = result.rows.map( row => row.item ) || [];
 			}
 		});
 	});
 
+  console.log('Bot is ready');
 });
 
 bot.on('message', function (message) {
-	//console.log(`User: ${user} on channel ${channelID} with message ${message}. `);
-  console.log("####################################### Current Channel ###################################")
-  console.log(message.channel);
-  console.log(message.channel.nsfw);
+  console.log(`### Channel '${message.channel.name}' is considered a NSFW channel: ${message.channel.nsfw} ###`);
 
-    if (message.content.substring(0, 1) == '!') {
-        var args = message.content.substring(1).split(' '); //remove ! and split args
-        var cmd = args[0];
+  const prefix = '!';
 
-        const param = args.splice(1, 1)[0]; //keep on splicing to get more args
-        switch(cmd) {
-            case 'ping':
-              message.channel.send(`:ping_pong:\n*${Date.now() - message.createdTimestamp} ms*`);
-            break;
-      			case 'flush':
-      				if (param){
-      					delete cache[param];
-      				} else {
-      					cache = {};
-      				}
-              message.channel.send(`Cache Flushed, these links be fresh now`);
-            break;
-      			case 'nsfw':
-      				if (param){
-      					if (param === "on"){
-      						allowNSFW = true;
-      					} else if (param === "off"){
-      						allowNSFW = false;
-      					} else if (param === "toggle"){
-      						allowNSFW = !allowNSFW;
+  if (
+    message.author.bot || //filter out bot responses
+    !message.content.startsWith(prefix) //filter out non-prefix messages
+    )
+    return;
+
+    var args = message.content.substring(1).split(' '); //remove ! and split args
+    var cmd = args[0].toLowerCase();
+
+    const param = args.splice(1, 1)[0]; //keep on splicing to get more args
+    switch(cmd) {
+        case 'ping':
+          message.channel.send(`:ping_pong:\n*${Date.now() - message.createdTimestamp} ms*`);
+        break;
+  			case 'flush':
+  				if (param){
+  					delete cache[param];
+  				} else {
+  					cache = {};
+  				}
+          message.channel.send(`Cache Flushed, these links be fresh now`);
+        break;
+  			case 'nsfw':
+  				if (param){
+  					if (param === "on"){
+  						allowNSFW = true;
+  					} else if (param === "off"){
+  						allowNSFW = false;
+  					} else if (param === "toggle"){
+  						allowNSFW = !allowNSFW;
+  					}
+  				}
+          message.channel.send(`NSFW is currently set to ${(allowNSFW) ? "On, nsfw links will be shown" : "Off, nsfw links will be hidden"}.`);
+        break;
+        case 'img':
+  				let subreddit = param.toLowerCase();
+  				if (!subreddit){
+            message.channel.send("**Must specify a subreddit!**\nEx:\n!img subreddit_name --> !img gifs");
+  					return;
+  				}
+
+  				if (allowNSFW === false && restrictList.includes(subreddit) ){
+            message.channel.send("*Please check yourself before you wreck yourself*");
+  					return;
+  				}
+
+          //get from cache if available
+  				if (cache[subreddit] && cache[subreddit].links.length > 0){
+            message.channel.send(getRandImg(cache[subreddit], message.channel.nsfw));
+            return;
+  				}
+
+          //make new request to imgur
+          var imgurSubredditPicOptions = {
+      		    uri: `https://api.imgur.com/3/gallery/r/${subreddit}`,
+      		    headers: {
+      		        'Authorization': `Client-ID ${clientId}`
+      		    },
+      		    json: true // Automatically parses the JSON string in the response
+      		};
+
+          rp(imgurSubredditPicOptions)
+      		    .then(function (JSONdata) {
+      					let botMessage = "";
+      					if (JSONdata.data.length !== 0){
+      						cache[subreddit] = makeCacheObject(JSONdata);
+      						if (cache[subreddit].links.length < 50)
+      							botMessage = `**Warning! Only ${cache[subreddit].links.length} image(s) associated with the __${subreddit}__ SubReddit imgur**\n`
+      						botMessage += getRandImg(cache[subreddit], message.channel.nsfw || message.channel.name.toLowerCase().includes('nsfw'));
+      					} else {
+      						botMessage = `Error: no images for subreddit '${subreddit}' found`;
       					}
-      				}
-              message.channel.send(`NSFW is currently set to ${(allowNSFW) ? "On, nsfw links will be shown" : "Off, nsfw links will be hidden"}.`);
-            break;
-            case 'img':
-      				let subreddit = param.toLowerCase();
-      				if (!subreddit){
-                message.channel.send("**Must specify a subreddit!**\nEx:\n!img subreddit_name --> !img gifs");
-      					return;
-      				}
 
-      				if (allowNSFW === false && restrictList.includes(subreddit) ){
-                message.channel.send("*Please check yourself before you wreck yourself*");
-      					return;
-      				}
-
-              //get from cache if available
-      				if (cache[subreddit] && cache[subreddit].links.length > 0){
-                message.channel.send(getRandImg(cache[subreddit], message.channel.nsfw));
-      				}
-
-      				const options = {
-      					hostname: 'api.imgur.com',
-      					path: `/3/gallery/r/${subreddit}`,
-      					headers: {'Authorization': `Client-ID ${clientId}`}
-      				};
-      				let body = [];
-
-      				https.get(options, (res) => {
-      				  res.on('data', (d) => {body.push(d); })
-      				  .on('end', () => {
-        					body = Buffer.concat(body).toString();
-
-        					//check for response body contents
-        					let rawBodyInfo = JSON.parse(body);
-        					let botMessage = "";
-        					if (rawBodyInfo.data.length > 0){
-        						cache[subreddit] = makeCacheObject(rawBodyInfo);
-        						if (cache[subreddit].links.length < 50)
-        							botMessage = `**Warning! Only ${cache[subreddit].links.length} image(s) associated with the __${subreddit}__ SubReddit imgur**\n`
-        						botMessage += getRandImg(cache[subreddit], message.channel.nsfw);
-        					} else {
-        						botMessage = `Error: no images for subreddit '${subreddit}' found`;
-        					}
-
-                  message.channel.send(botMessage);
-
-					      }); //end http msg received
-      				}).on('error', (e) => {
-      				  console.error(e);
-      				}); //end http request
-            break;
-         } //end switch
-     }
-});
+                message.channel.send(botMessage);
+      		    })
+      		    .catch(function (err) {
+      		        console.log(err);
+              });
+        break;
+        case 'imgbulk':
+          let amount = parseInt(param);
+          if (!isNaN(amount)){
+            //process request
+          }
+        break;
+     } //end switch command parsing
+}); //end bot.onMessage
 
 
 bot.login(auth.token);
