1a5a37b10499909b47dea1fc178016743adbe560
Keung Huang <notinschool@gmail.com>
2017-10-01 00:40:53 -0700

    Changed ordering of youtube playing logic

--- bot.js
+++ bot.js
@@ -1,21 +1,19 @@
-const Discord = require('discord.js');
-const rp = require('request-promise');
-const pg = require('pg');
-const YouTube = require('youtube-node');
-const ytdl = require('ytdl-core');
+const Discord = require("discord.js");
+const rp = require("request-promise");
+const YouTube = require("youtube-node");
+const ytdl = require("ytdl-core");
 
-const auth = require('./auth.json');
+const settings = require("./settings.json");
 
 const bot = new Discord.Client();
 const youTube = new YouTube();
 
-youTube.setKey(auth.YOUTUBE_API_KEY);
+youTube.setKey(settings.YOUTUBE_API_KEY);
 
 // Initialize Discord Bot
-const clientId = process.env.IMGUR_CLIENT_ID || auth.IMGUR_CLIENT_ID;
+const clientId = process.env.IMGUR_CLIENT_ID || settings.IMGUR_CLIENT_ID;
 let allowNSFW = false;
 let cache = {};
-let restrictList = [];
 
 //got this function from Mozilla Dev Net.
 function getRandomInt(min, max) {
@@ -56,50 +54,35 @@ function imgurSubredditPromise(subreddit){
   var imgurSubredditPicOptions = {
       uri: `https://api.imgur.com/3/gallery/r/${subreddit}`,
       headers: {
-          'Authorization': `Client-ID ${clientId}`
+          "Authorization": `Client-ID ${clientId}`
       },
       json: true // Automatically parses the JSON string in the response
   };
   return rp(imgurSubredditPicOptions);
 }
 
-bot.on('ready', () => {
-  //connect to database
-	pg.connect(process.env.DATABASE_URL , function(err, client, done) {
-		client.query('SELECT item FROM restrict_list', function(err, result) {
-		  done();
-		  if (err){
-        console.error(err);}
-		  else {
-        restrictList = result.rows.map( row => row.item ) || [];
-        console.log(restrictList);
-			}
-		});
-	});
-
-  console.log('Bot is ready');
+bot.on("ready", () => {
+  console.log("Bot is ready");
 });
 
-bot.on('message', function (message) {
-  console.log(`### Channel '${message.channel.name}' is considered a NSFW channel: ${message.channel.nsfw} ###`);
-
-  const prefix = '!';
+bot.on("message", function (message) {
+  console.log(`### Channel "${message.channel.name}" is considered a NSFW channel: ${message.channel.nsfw} ###`);
 
   if (
     message.author.bot || //filter out bot responses
-    !message.content.startsWith(prefix) //filter out non-prefix messages
+    !message.content.startsWith(settings.prefix) //filter out non-prefix messages
     )
     return;
 
-    var args = message.content.substring(1).split(' '); //remove ! and split args
+    var args = message.content.substring(1).split(" "); //remove ! and split args
     var cmd = args.splice(0,1).toString().toLowerCase();
 
     const param = args.splice(0, 1)[0]; //keep on splicing to get more args
     switch(cmd) {
-        case 'ping':
+        case "ping":
           message.channel.send(`:ping_pong:\n*${Date.now() - message.createdTimestamp} ms*`);
         break;
-  			case 'flush':
+  			case "flush":
   				if (param){
   					delete cache[param];
   				} else {
@@ -107,7 +90,7 @@ bot.on('message', function (message) {
   				}
           message.channel.send(`Cache Flushed, these links be fresh now`);
         break;
-  			case 'nsfw':
+  			case "nsfw":
   				if (param){
   					if (param === "on"){
   						allowNSFW = true;
@@ -119,15 +102,10 @@ bot.on('message', function (message) {
   				}
           message.channel.send(`NSFW is currently set to ${(allowNSFW) ? "On, nsfw links will be shown" : "Off, nsfw links will be hidden"}.`);
         break;
-        case 'img':
+        case "img":
   				let subreddit = param.toLowerCase();
   				if (!subreddit){
             message.channel.send("**Must specify a subreddit!**\nEx:\n!img subreddit_name --> !img gifs");
-  					return;
-  				}
-
-  				if (allowNSFW === false && restrictList.includes(subreddit) ){
-            message.channel.send("*Please check yourself before you wreck yourself*");
   					return;
   				}
 
@@ -145,9 +123,9 @@ bot.on('message', function (message) {
       						cache[subreddit] = makeCacheObject(JSONdata);
       						if (cache[subreddit].links.length < 50)
       							botMessage = `**Warning! Only ${cache[subreddit].links.length} image(s) associated with the __${subreddit}__ SubReddit imgur**\n`
-      						botMessage += getRandImg(cache[subreddit], message.channel.nsfw || message.channel.name.toLowerCase().includes('nsfw'));
+      						botMessage += getRandImg(cache[subreddit], message.channel.nsfw || message.channel.name.toLowerCase().includes("nsfw"));
       					} else {
-      						botMessage = `Error: no images for subreddit '${subreddit}' found`;
+      						botMessage = `Error: no images for subreddit "${subreddit}" found`;
       					}
 
                 message.channel.send(botMessage);
@@ -156,36 +134,40 @@ bot.on('message', function (message) {
       		        console.log(err);
               });
         break;
-        case 'imgbulk':
+        case "imgbulk":
           let amount = parseInt(param);
           if (!isNaN(amount)){
             message.channel.send("Under construction, please wait while Ken works on this eventually");
           }
         break;
-        case 'yt':
+        case "yt":
           const voiceChannel = message.member.voiceChannel;
       		if (!voiceChannel){
       			return message.reply(`Please join a voice channel first!`); //mentions the user
       		}
-          console.log("youTube search: " + param + " " + args.join(' '));
-      		youTube.search(param + " " + args.join(' '), 1, function(error, result) {
-      		  if (error) {
-      		    console.log(error);
-      		  }
-      		  else {
-      		    const link = result.items.map( item => item.id.videoId); //filterYoutubeVideoLinks(result);
-              console.log(link); //since this array only has 1 value, its toString method will return what we want
-      				voiceChannel.join()
-      					.then(voiceConnnection => {
-      						const stream = ytdl(`https://www.youtube.com/watch?v=${link}`, { filter: 'audioonly' });
-      						const dispatcher = voiceConnnection.playStream(stream);
-      						dispatcher.on('end', () => voiceChannel.leave());
-      					});
-      		  }
-    		});
+          console.log("youTube search: " + param + " " + args.join(" "));
+
+          voiceChannel.join()
+            .then(voiceConnnection => {
+              if (voiceConnnection.speaking){
+                return message.channel.send("You must suffer until this song ends")
+                  .then(msg => msg.delete(5000));
+              }
+              youTube.search(param + " " + args.join(" "), 1, function(error, result) {
+                if (error) {
+                  console.log(error);
+                }
+                else {
+                  const link = result.items.map( item => item.id.videoId); //filterYoutubeVideoLinks(result);
+                  const stream = ytdl(`https://www.youtube.com/watch?v=${link}`, { filter: "audioonly" });
+                  const dispatcher = voiceConnnection.playStream(stream);
+                  dispatcher.on("end", () => voiceChannel.leave());
+                }
+              }); //end youtube search and play
+            }); //end voice channel joining
         break;
      } //end switch command parsing
 }); //end bot.onMessage
 
 
-bot.login(auth.token);
+bot.login(settings.token);
